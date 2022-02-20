c571bdd00436b875b7f63cf116b5061dc371b020
Peter Swantek <swantek2@gmail.com>
2022-02-20 14:38:52 -0600

    test

--- appointment-management-system/src/app/app.component.ts
+++ appointment-management-system/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'appointment-management-system';
+  title = 'appointment-management-system!';
 }
