7e04b4f938a708fe88a846d94d8972cc98f96dda
Peter Swantek <swantek2@gmail.com>
2022-02-20 14:59:06 -0600

    add libraries

--- appointment-management-system/src/app/app.module.ts
+++ appointment-management-system/src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { NgxsModule } from '@ngxs/store';
+import { environment } from '../environments/environment.prod';
+
+
+FullCalendarModule.registerPlugins([
+  interactionPlugin,
+  timeGridPlugin
+]);
 
 @NgModule({
   declarations: [
@@ -10,7 +22,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FullCalendarModule,
+    NgxsModule.forRoot([], {developmentMode: !environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
